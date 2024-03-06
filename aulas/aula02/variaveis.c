62be577a1f674c7ac03f0faaa98c9785bba9837c
Goncalvesvini123 <160738056+Goncalvesvini123@users.noreply.github.com>
2024-03-06 13:58:38 +0000

    aula 2 codigos e praticas

---
+++ aulas/aula02/variaveis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() { 
+  int numero;
+  char caracter;
+  int ; // essa variavel nao existe
+  int x;
+  float nome_composto 
+
+  numero = 10 
+
+  numero = 100 
+  return 0;
+}
