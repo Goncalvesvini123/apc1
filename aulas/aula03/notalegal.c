ee59851c8c62d206d0b20b95d9a56de81f6b266c
Goncalvesvini123 <160738056+Goncalvesvini123@users.noreply.github.com>
2024-03-13 13:51:13 +0000

    codigos da aula 3 e pratica 2

---
+++ aulas/aula03/notalegal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+ int main() {
+  printf("----------------------------------\n");
+  printf("        N O T A   L E G A L       \n");
+  printf("----------------------------------\n");
+  printf("ITEM              QTD      VALOR  \n");
+  printf("%-16s %3i %11.2f\n" , "Banana", 1, 1.50);
+  printf("%-16s %3i %11.2f\n" , "Laranja", 5, 2.00);
+  printf("%-16s %3i %11.2f\n" , "qjo 500g", 2, 25.00);
+  printf("%-16s %3i %11.2f\n" , "pizza sadia", 2, 35.90);
+  printf("----------------------------------\n");
+  printf("TOTAL              11              145.30\n");
+
+  return 0;
+ }
