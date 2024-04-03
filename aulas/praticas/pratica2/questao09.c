c647637a63af1a09a25e1c6ef1001b970f75484e
Goncalvesvini123 <160738056+Goncalvesvini123@users.noreply.github.com>
2024-04-03 13:14:26 +0000

    atualizaão das praticas

---
+++ aulas/praticas/pratica2/questao09.c
@@ -0,0 +1,17 @@
+/*
+9. Faça um programa em C que leia um número inteiro e imprima o caractere correspondente na tabela ASCII.
+*/
+
+#include <stdio.h>
+
+int main() {
+  int numero;
+  int ASCII;
+
+  printf("Insira um número inteiro: ");
+  scanf("%d", &numero);
+
+  printf("O caractere correspondente na tabela ASCII é: %c\n", numero);
+
+  return 0;
+}
